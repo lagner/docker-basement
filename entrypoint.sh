1659c267daba25f4f219a38230779b38feac6270
Sergey Lagner <spam@lagner.ru>
2016-11-05 10:29:49 +0300

    console prompt was changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,30 @@
 
 /usr/games/cowsay -f beavis.zen "I'm BATMAN!"
 
+cat << EOF >> $HOME/.bashrc
+
+set_prompt () {
+    Last_Command=\$? # Must come first!
+    Blue='\[\e[01;34m\]'
+    Red='\[\e[01;31m\]'
+    Green='\[\e[01;32m\]'
+    Reset='\[\e[00m\]'
+    FancyX='\342\234\227'
+    Checkmark='\342\234\223'
+
+    PS1=""
+
+    if [[ \$Last_Command == 0 ]]; then
+        PS1+="\$Green\$Checkmark "
+    else
+        PS1+="\$Red\$FancyX "
+    fi
+    PS1+="\$Green(DOCKER) \$Blue\\w \\\$\$Reset "
+}
+
+PROMPT_COMMAND='set_prompt'
+EOF
+
 cd $workdir
 
 /bin/bash -c "$@"
